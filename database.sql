b6b5f6ea77ade7db80a96b376fdac9c9e42c7445
Johnshandsable <Jshands@asu.edu>
2021-03-15 10:52:47 -0500

    initial commit

--- database.sql
+++ database.sql
@@ -6,5 +6,6 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "location" VARCHAT(80) NOT NULL
 );
